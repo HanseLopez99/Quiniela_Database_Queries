3d6eaf14f902e0aa2f4a0498ba0ee77e1ea9836f
Hansel López <halm29799@gmail.com>
2022-10-12 09:50:33 -0600

    feat: add queries for games

--- queries.sql
+++ queries.sql
@@ -3,3 +3,11 @@ SELECT description FROM phases;
 
 -- Teams view
 SELECT name FROM teams;
+
+-- Games view
+SELECT t1.name as "Team 1", t2.name as "Team 2", g.starting_at as "Starting at", p.description as "Phase", g.team1_score as "Score 1", g.team2_score as "Score 2"
+FROM games g 
+INNER JOIN teams t1 on t1.id = g.team1_id 
+INNER JOIN teams t2 on t2.id = g.team2_id 
+INNER JOIN phases p on p.id = g.phases_id 
+ORDER BY g.created_at ASC;
